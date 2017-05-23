d12d5c428259ea2a3b2c135ccfb1d1d8a8b6bbfb
Thibaud <thibaudmaneval96@gmail.com>
2017-05-23 17:32:26 +0200

    .h

--- Projet/headers/decompression.h
+++ Projet/headers/decompression.h
@@ -4,5 +4,5 @@
 #include "dict.h"
 void decodage (char* fichier,char* sortie);
 int* concat(int* a, int longa, int* b, int longb);
-void ecriture(File* s,int* c,int longc);
+void ecriture(FILE* s,int* c,int longc);
 #endif
