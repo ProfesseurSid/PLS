796373b5b6cd55f5c6d7b12628a73d8c6c6d33a9
Thibaud <thibaudmaneval96@gmail.com>
2017-05-23 16:05:21 +0200

    ifndef

--- Projet/headers/decompression.h
+++ Projet/headers/decompression.h
@@ -1,5 +1,8 @@
-#include "decompression.h"
+#ifndef _DECOMPRESSION_H
+#define _DECOMPRESSION_H
+
 #include "dict.h"
 void decodage (char* fichier,char* sortie);
 int* concat(int* a, int longa, int* b, int longb);
 void ecriture(File* s,int* c,int longc);
+#endif
