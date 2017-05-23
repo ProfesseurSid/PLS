da5c6661daefcd8f58e69517da40fbebef312b65
gmDefault <cedric.lafrasse@gmail.com>
2017-05-23 16:47:15 +0200

    JPP (Jean-Pierre Pernault)

--- Projet/src/main.c
+++ Projet/src/main.c
@@ -4,83 +4,7 @@
 
 #include "compression.h"
 #include "decompression.h"
-//#include "piece.h"
-//#include "coup.h"
-//#include "plateau.h"
-//#include "vt100.h"
-//#include "fonc_carac.h"
 
-/*
-void saisie_clavier(char langue){
-	affiche_func_param_t sortie;
-	char coup[4];
-	echiquier_t plateau;
-	couleur_t dernierJoueur = Noir;
-
-	init(plateau);
-	afficher_plateau(plateau, sortie, langue);
-	switch(langue){
-		case('A'): printf("Give your move (column-line-column-line) : ");break;
-		default: printf("Entrez un coup (colonne-ligne-colonne-ligne) : ");
-	}
-
-	do{
-		fscanf(stdin, "%s", coup);
-		if(feof(stdin))
-			switch(langue){
-				case('A'): printf("Good bye !\n");break;
-				default: printf("Au revoir !\n");
-			}
-		else if(creer_coup_interact(coup, dernierJoueur, plateau)){
-			afficher_plateau(plateau, sortie, langue);
-			switch(langue){
-				case('A'): printf("Next move : ");break;
-				default: printf("Coup suivant : ");
-			}
-			// On change de joueur
-			if(dernierJoueur == Blanc)
-				dernierJoueur = Noir;
-			else
-				dernierJoueur = Blanc;
-		}
-		else
-			switch(langue){
-				case('A'): printf("\nWrong move. Next move : ");break;
-				default: printf("\nMauvais coup. Coup suivant : ");
-			}
-	}while(!feof(stdin));
-
-	printf("\n");
-
-}
-
-void lire_fichier(FILE *f, char langue)
-{
-	char coup[4];
-	char trash;
-	coup_t *ListeDeCoups = NULL;
-	coup_t *QueueListeDeCoups = NULL;
-	echiquier_t plateau;
-	affiche_func_param_t sortie;
-
-	init(plateau);
-	afficher_plateau(plateau, sortie, langue);
-
-	while(!feof(f))
-	{
-		fscanf(f,"%c%c%c%c%c",&coup[0],&coup[1],&coup[2],&coup[3],&trash);
-		creer_coup(coup, &ListeDeCoups, &QueueListeDeCoups);
-	}
-
-	init(plateau);
-	while(ListeDeCoups != NULL){
-		printf("    %s\n\n", ListeDeCoups->comment);
-		jouer_coup(plateau, &ListeDeCoups);
-		afficher_plateau(plateau, sortie, langue);
-		scanf("%c",&trash);
-	}
-}
-*/
 int main(int argc, char *argv[]){
 
 	//FILE *f = NULL;
@@ -92,7 +16,7 @@ int main(int argc, char *argv[]){
 			switch(argv[1][1]){
 				case('h'):
 					printf("NAME\n\tLZW compressor - file compressor using lzw algorithm\n");
-					printf("\nSYNOPSIS\n\tchess [-cd] [val [...]]\n");
+					printf("\nSYNOPSIS\n\t lzw [-cd] [val [...]]\n");
 					printf("\nLISTING OPTIONS\n\t-c\tCompresses the given file(s).\n\t\tCompressed file is <file name>.lzw.\n\t-d\tDecompresses the given file(s).\n\t\tFiles MUST be files compressed with .lzw extension.\n\t\tOtherwise, does not even try.\n\t-h\tDisplays this help.\n");
 					//HELP = 1;
 					break;
